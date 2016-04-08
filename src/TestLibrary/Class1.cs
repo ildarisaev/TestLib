83acc6009b901bc47aa470f92c6d5f23fa26610a
Ildar Isaev <iisaev@codeminders.com>
2016-04-08 14:57:21 +0300

    foo

--- src/TestLibrary/Class1.cs
+++ src/TestLibrary/Class1.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace TestLibrary
 {
-    // This project can output the Class library as a NuGet Package.
-    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
-    public class Class1
+    Class A { }
+  
+    Class B { }
+
+    Class C
     {
-        public void LibFunc()
+        public void foo()
         {
-            Console.WriteLine("This is test function from library");
+            A tgr;
         }
     }
 }
